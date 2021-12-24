bd376c190a247b45a31f7e06914feaa21ff24550
Tonia Saba <Tonia.saba@gmail.com>
2021-12-24 20:32:38 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,17 @@
+def first_palindrome(words)
+  words.each do |word|
+    if word.reverse == word 
+      puts word 
+      break
+    else
+      puts ""
+    end 
+  end
+end
+
+first_palindrome(["abc","car","ada","racecar","cool"])
+
+
+
+
+  # words.reverse == words ? puts("Is a Pali") : puts("is not")
