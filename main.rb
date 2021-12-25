72d785e7f8e7bd631e4669b9218794f36cf353d1
Tonia Saba <Tonia.saba@gmail.com>
2021-12-25 03:30:45 +0000

    solves first_palindrome

--- main.rb
+++ main.rb
@@ -1,17 +1,20 @@
 def first_palindrome(words)
-  words.each do |word|
-    if word.reverse == word 
-      puts word 
-      break
-    else
-      puts ""
-    end 
+  words.each do |word| 
+      puts word if word.reverse == word
   end
+  puts ""
 end
 
-first_palindrome(["abc","car","ada","racecar","cool"])
+first_palindrome(["abc", "cool"])
 
 
 
 # is palendrome
   # words.reverse == words ? puts("Is a Pali") : puts("is not")
+
+#   def first_palindrome(words)
+#     for i in 0..words.length-1
+#         return words[i] if words[i] == words[i].reverse
+#     end
+#     ""
+# end
